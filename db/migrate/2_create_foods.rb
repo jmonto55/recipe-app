6fd391c84a1466bfbcd6b01fe20e639721195e5c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-22 15:32:50 -0400

    Create migration file for food model

--- db/migrate/2_create_foods.rb
+++ db/migrate/2_create_foods.rb
@@ -1,6 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
+      t.string :name
+      t.float :measuremnet_unit
+      t.float :price
+      t.integer :quantity
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
