5f47fbcbd4e8506f7e3aad9bfa1f670526648a59
jmonto55 <jmonto55@gmail.com>
2023-05-22 14:02:04 -0500

    Add relationships between Models and methods to increment counters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods, foreign_key: :recipe_id
+
+  attribute :recipe_foods_counter, :integer, default: 0
+
+  def update_recipes_counter
+    user.increment!(:recipes_counter)
+  end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,2 +1,8 @@
 class RecipeFood < ApplicationRecord
+  belongs_to :food
+  belongs_to :recipe
+
+  def update_recipe_foods_counter
+    user.increment!(:recipe_foods_counter)
+  end
 end
