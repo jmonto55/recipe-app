bd802e562e8d91a45f2634c48a530fa832d5612a
jmonto55 <jmonto55@gmail.com>
2023-05-22 13:52:18 -0500

    Build migration files for Recipe and RecipeFood Models

--- db/migrate/4_create_recipe_foods.rb
+++ db/migrate/4_create_recipe_foods.rb
@@ -1,6 +1,11 @@
+# db/migrate/20230523000000_create_recipe_foods.rb
+
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
+      t.float :quantity
+      t.references :recipe, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
 
       t.timestamps
     end
