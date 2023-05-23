547e64d74a7cfc45f90d15879e7fb18105ffa5aa
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-23 18:55:37 -0400

    Add function to hande food creation

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,14 +21,11 @@ class FoodsController < ApplicationController
   def create
     @food = Food.new(food_params)
 
-    respond_to do |format|
-      if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
-        format.json { render :show, status: :created, location: @food }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -64,6 +61,6 @@ class FoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.fetch(:food, {})
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
