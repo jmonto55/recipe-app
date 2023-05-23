7145c453bd0af743e9757aecc8c52e15f1d23c8b
jmonto55 <jmonto55@gmail.com>
2023-05-23 13:29:39 -0500

    Add navigation bar

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /foods or /foods.json
   def index
