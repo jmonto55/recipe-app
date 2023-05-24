05af8776c01300af4872bb561397c73e4da5935f
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-23 21:09:39 -0400

    Remove unused code

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,12 +2,12 @@ class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
-  # GET /foods or /foods.json
+  # GET /foods
   def index
     @foods = Food.all
   end
 
-  # GET /foods/1 or /foods/1.json
+  # GET /foods/1
   def show; end
 
   # GET /foods/new
@@ -18,7 +18,7 @@ class FoodsController < ApplicationController
   # GET /foods/1/edit
   def edit; end
 
-  # POST /foods or /foods.json
+  # POST /foods
   def create
     @food = current_user.foods.build(food_params)
 
@@ -29,27 +29,10 @@ class FoodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /foods/1 or /foods/1.json
   def destroy
     @food.destroy
 
-    respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to foods_path, notice: 'Food was successfully destroyed.'
   end
 
   private
