4b38abc12cd17e985d987de18e1e5c09430a2594
jmonto55 <jmonto55@gmail.com>
2023-05-24 19:02:47 -0500

    Add neumorphic styles to login view

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        neu1: '-5px -5px 15px 2px rgb(0 0 0 / 0.1)',
+        neu2: '-5px -5px 10px 2px rgb(0.9 0.9 0.9 / 0.1)',
+        innerneu1: 'inset -5px -5px 10px 2px rgb(0.9 0.9 0.9 / 1)',
+        innerneu2: 'inset -5px -5px 15px 2px rgb(0 0 0 / 0.1)'
+      },
     },
   },
   plugins: [
