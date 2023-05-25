7bae5a9d919719c6681e4d7d8bc57ac1a395e7a8
jmonto55 <jmonto55@gmail.com>
2023-05-24 22:29:37 -0500

    Add validations Recipe Model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, foreign_key: :recipe_id
-
+  
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
+  
   attribute :recipe_foods_counter, :integer, default: 0
 end
