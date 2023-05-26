64ba8973377178ee2e63c3497cf32ccdf2d69453
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-26 14:29:20 -0400

    Add logic to generate shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,21 +12,11 @@ class RecipesController < ApplicationController
 
   def general_shopping_list
     user_recipes = current_user.recipes
-
     user_foods = current_user.foods
-
     recipe_foods = RecipeFood.where(recipe_id: user_recipes.pluck(:id))
+    recipe_food_quantity = get_recipe_foods_quantity(recipe_foods)
 
-    puts 'recipe_foods', recipe_foods.inspect
-
-    puts '======================'
-
-    puts 'user_foods', user_foods.inspect
-
-    recipe_food_names = recipe_foods.pluck(:id)
-
-    puts '======================'
-    puts 'recipe_food_names', recipe_food_names.inspect
+    get_general_shopping_list(user_foods, recipe_food_quantity)
   end
 
   def add_food
@@ -82,5 +72,40 @@ class RecipesController < ApplicationController
   # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+
+  # generate general shopping list
+
+  def get_recipe_foods_quantity(recipe_foods)
+    recipe_food_quantity = {}
+    recipe_foods.includes(:food).each do |food|
+      name = food.food.name
+      if recipe_food_quantity[name].nil?
+        recipe_food_quantity[name] = food.quantity
+      else
+        recipe_food_quantity[name] += food.quantity
+      end
+    end
+
+    recipe_food_quantity
+  end
+
+  def get_general_shopping_list(user_foods, recipe_food_quantity)
+    @general_shopping_list = []
+    user_foods.each do |food|
+      name = food.name
+      quantity = food.quantity
+      price = food.price
+      next unless recipe_food_quantity[name]
+
+      r_quantity = recipe_food_quantity[name]
+      next unless quantity < r_quantity
+
+      g_quantity = r_quantity - quantity
+      g_price = g_quantity * price
+      @general_shopping_list << { name:, quantity: g_quantity, price: g_price }
+    end
+
+    @general_shopping_list
   end
 end
