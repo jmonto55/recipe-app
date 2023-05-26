d717820af318f1a0d6c43618a1f81b259142cb5e
jmonto55 <jmonto55@gmail.com>
2023-05-26 12:29:03 -0500

    Build tests for recipes and recipe_foods controllers

---
+++ spec/requests/recipes_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes', type: :request do
+  describe 'GET /index' do
+    before(:example) do
+      user = User.create(name: 'Tom', email: 'tom@mail.com')
+      recipe = Recipe.create(
+        name: 'rice with shrimp',
+        preparation_time: 1,
+        cooking_time: 2,
+        description: 'cook shrimp and mix with rice and vegetables',
+        public: true,
+        user_id: user.id
+      )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      get "/recipes"
+    end
+
+    it 'should return success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return a 200 response status code' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'should render index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'include the correct text' do
+      expect(response.body).to include('Recipes')
+    end
+  end
+end
