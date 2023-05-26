a9dd973799ef26bc82eb114c15c03986bd6b12ae
jmonto55 <jmonto55@gmail.com>
2023-05-25 23:15:15 -0500

    Fix last n + 1 issue

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -13,7 +13,7 @@ class RecipeFoodsController < ApplicationController
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.new
-    @foods = Food.all
+    @foods = current_user.foods
   end
 
   # GET /recipe_foods/1/edit
