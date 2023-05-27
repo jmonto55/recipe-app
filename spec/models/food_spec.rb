0517226dd319d450f1f565722f61b4bc7bba2706
jmonto55 <jmonto55@gmail.com>
2023-05-26 20:09:45 -0500

    Build specs for Models User and Food

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Food, type: :model do
+RSpec.describe Recipe, type: :model do
   let(:user) { User.new(name: 'Tom', email: 'tom@mail.com') }
-
   let(:food) do
     Food.create(
       name: 'Beans',
@@ -11,5 +10,19 @@ RSpec.describe Food, type: :model do
       quantity: 500,
       user_id: user.id
     )
+  end
+
+  describe 'attributes' do
+    it 'has a name' do
+      expect(food.name).to eq('Beans')
+    end
+
+    it 'has "grams" as measurement unit' do
+      expect(food.measurement_unit).to eq('grams')
+    end
+
+    it 'has a price of 10' do
+      expect(food.price).to eq(10.0)
+    end
   end
 end
